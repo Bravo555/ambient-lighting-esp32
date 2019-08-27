4472a28b67687b48daf6304d9c82b83cfc765dde
Marcel Guzik <mguzik555@gmail.com>
2019-08-27 16:43:21 +0200

    fixed indentation

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,12 @@ void setup() {
 void loop() {
     for(int i=0; i<NUMPIXELS * 4; i++) { // For each pixel...
 
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i % 60, pixels.Color((i * 10)%250, 150, (i-10)%250));
+        // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+        // Here we're using a moderately bright green color:
+        pixels.setPixelColor(i % 60, pixels.Color((i * 10)%250, 150, (i-10)%250));
 
-    pixels.show();   // Send the updated pixel colors to the hardware.
+        pixels.show();   // Send the updated pixel colors to the hardware.
 
-    delay(DELAYVAL); // Pause before next pass through loop
-  }
+        delay(DELAYVAL); // Pause before next pass through loop
+    }
 }
