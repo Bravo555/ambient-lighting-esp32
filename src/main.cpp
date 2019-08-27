706d76001fc51e958a09d0301e8702b11646b8e6
Marcel Guzik <mguzik555@gmail.com>
2019-08-27 17:17:53 +0200

    added setting brightness by serial port
    Works by sending `\x00 [brightness]` to serial port.
    In case only the first byte is sent, program ignores the command.

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,29 @@ const int DELAYVAL = 200;
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
+    Serial.begin(9600);
     pixels.begin();
 }
 
 void loop() {
-    for(int i=0; i<NUMPIXELS * 4; i++) { // For each pixel...
+    for(int i=0; i<NUMPIXELS * 4; i++) {
+        while(Serial.available()) {
+            uint8_t data = Serial.read();
+            if(data == 0x00) {
+                // set brightness
+                int brightness = Serial.read();
+                if(brightness == -1) break;
+                Serial.printf("setting brightness at: %d\n", brightness);
+                pixels.setBrightness(brightness);
+            }
+        }
 
         // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-        // Here we're using a moderately bright green color:
         pixels.setPixelColor(i % 60, pixels.Color((i * 10)%250, 150, (i-10)%250));
 
         pixels.show();   // Send the updated pixel colors to the hardware.
 
         delay(DELAYVAL); // Pause before next pass through loop
+                         // need to get rid of this
     }
 }
