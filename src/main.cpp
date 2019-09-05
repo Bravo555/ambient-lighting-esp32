6c62d5dd2b3cfce2deb4151a8f1d268cd54f5eb3
Marcel Guzik <mguzik555@gmail.com>
2019-09-05 17:38:05 +0200

    transferring single pixel

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@ void setup() {
 }
 
 void loop() {
-    for(int i=0; i<NUMPIXELS * 4; i++) {
         while(Serial.available()) {
             uint8_t data = Serial.read();
             if(data == 0x00) {
@@ -23,14 +22,27 @@ void loop() {
                 if(Serial.availableForWrite()) Serial.printf("setting brightness at: %d\n", brightness);
                 pixels.setBrightness(brightness);
             }
+
+            if(data == 0x01) { // incoming colour data
+                byte colours[3] = {0x12, 0x34, 0x56};
+                Serial.readBytes(colours, 3);
+
+                Serial.write(colours[0]);
+                Serial.write(colours[1]);
+                Serial.write(colours[2]);
+
+                for(int i = 0; i < NUMPIXELS; i++) {
+                    pixels.setPixelColor(i, pixels.Color(colours[0], colours[1], colours[2]));
+                }
+                pixels.show();
+            }
+
+            if((char)data == 'A') {
+                Serial.println("fuck off");
+            }
         }
 
         // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-        pixels.setPixelColor(i % 60, pixels.Color((i * 10)%250, 150, (i-10)%250));
 
-        pixels.show();   // Send the updated pixel colors to the hardware.
-
-        delay(DELAYVAL); // Pause before next pass through loop
-                         // need to get rid of this
-    }
+        //pixels.show();   // Send the updated pixel colors to the hardware.
 }
