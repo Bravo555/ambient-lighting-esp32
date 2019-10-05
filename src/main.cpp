5bcba385a66868ec2c5d68b77cc1011d13e82d7e
Marcel Guzik <mguzik555@gmail.com>
2019-10-05 17:27:55 +0200

    Applied tips to the circuit
    Leaving the comment for reference

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 // TODO:
 // NEOPIXEL BEST PRACTICES for most reliable operation:
-// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
-// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
-// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
+// - [x] Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
+// - [ ] MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
+// - [x] NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
 // - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
 //   connect GROUND (-) first, then +, then data.
 // - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
